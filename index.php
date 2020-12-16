fa21f7a224acc3962f0ffdb9e000e87fb1cf8a2c
abir <61460970+abirm74@users.noreply.github.com>
2020-12-16 23:23:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,271 @@
+<?php 
+session_start();
+
+include 'functions.php';
+
+if(empty($_SESSION['currency']) || $_SESSION['currency']=="USD")
+{
+	$_SESSION['currency']="USD";
+	$_SESSION['c_from']= "BDT";
+	$_SESSION['c_to']= "USD";
+	$_SESSION['c_symbol']= "$";
+}
+else if($_SESSION['currency']=="BDT")
+{
+	$_SESSION['c_from']= "USD";
+	$_SESSION['c_to']= "BDT";
+    $_SESSION['c_symbol']= "৳";
+}	
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Atlas travel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link rel="stylesheet" href="Admin/dist/css/public.css" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+
+<?php include 'nav.php'; ?>
+
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/cover-img-4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h2>Atlas travel </h2>
+				   					<h1>Welcome to our travel agency </h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/blog-4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h2>Your favourite agency </h2>
+				   					<h1>Travel around the world </h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+		<div id="colorlib-services">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-3 animate-box text-center aside-stretch">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-around"></i>
+							</span>
+							<h3>Amazing Travel</h3>
+							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies</p>
+						</div>
+					</div>
+					<div class="col-md-3 animate-box text-center">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-resort"></i>
+							</span>
+							<h3>Luxurious Hotels</h3>
+							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies</p>
+						</div>
+					</div>
+					<div class="col-md-3 animate-box text-center">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-car"></i>
+							</span>
+							<h3>Book Your Trip</h3>
+							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies</p>
+						</div>
+					</div>
+					<div class="col-md-3 animate-box text-center">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-postcard"></i>
+							</span>
+							<h3>Nice Support</h3>
+							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+<!--REEEEEEEEEEECEEEEEEEEEEEEENTTTTTTTT BLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOGGGGGG-->
+		<div id="colorlib-blog">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+						<h2>Recent Blog</h2>
+						<p>We love to tell our successful far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					</div>
+				</div>
+				<div class="blog-flex">
+					<div class="f-entry-img" style="background-image: url(images/cover-img-21.jpg);">
+					</div>
+					<div class="blog-entry aside-stretch-right">
+						<div class="row">
+			                <?php  
+							   require 'config.php';
+
+			                    $statement="select * from posts where deletedAt is null order by addedOn asc LIMIT 3";
+			                    $res_data = mysqli_query($conn, $statement);
+
+			                    if (mysqli_num_rows($res_data) > 0)
+			                    {
+			                        while($row = mysqli_fetch_assoc($res_data))
+			                        {
+			                      		$cat_title_query= mysqli_query($conn, "SELECT title from categories WHERE cat_id= $row[cat_id]");
+			                     		$cat_title= mysqli_fetch_assoc($cat_title_query);
+			                     		$title= $cat_title['title'];
+
+			                     		$timeStamp = $row['addedOn'];
+										$timeStamp = date( "m/d/Y", strtotime($timeStamp));
+
+										echo "<div class=\"col-md-12 animate-box\">";
+											echo "<a href=\"post-view.php?post_id=$row[post_id]\" class=\"blog-post\">";
+											 echo "<span class=\"img\" style=\"background-image: url(Admin/$row[image]);\"></span>";
+												echo "<div class=\"desc\">";
+													echo "<span class=\"date\">$timeStamp</span>";
+													echo "<h3>$row[title]</h3>";
+													echo "<span class=\"cat\">$title</span>";
+												echo "</div>";
+											echo "</a>";
+										echo "</div>";
+							        }
+			                    }
+			                    else
+			                    {
+			                        echo "Nothing found in db";
+			                    }
+			                    //mysqli_close($conn);
+			                ?>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--REEEEEEEEEEECEEEEEEEEEEEEENTTTTTTTT BLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOGGGGGG-->
+		
+<?php include 'footer.php'; ?>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+
+<!--Start of Tawk.to Script-->
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b82fb9aafc2c34e96e7eb01/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+	</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,42 @@
+					<!-- SIDEBAR-->
+					<div class="col-md-3">
+						<div class="sidebar-wrap">
+							
+
+
+							<div class="side animate-box">
+								<div class="row">
+									<div class="col-md-12">
+										<h3 class="sidebar-heading">Categories</h3>
+											<div class="list-group">
+
+								                <?php
+								                    require 'config.php';
+
+								                    $statement_cat="select * from categories where deletedAt is null order by cat_id asc";
+								                    $res_data_cat = mysqli_query($conn, $statement_cat);
+
+								                    if (mysqli_num_rows($res_data_cat) > 0)
+								                    {
+								                        while($row = mysqli_fetch_assoc($res_data_cat))
+								                        {
+
+											  				echo "<a href=\"categories-view.php?cat_id=$row[cat_id]\" class=\"list-group-item list-group-item-action\">$row[title]</a>";
+
+								                    	}	
+								                    }
+								                    else
+								                    {
+								                        echo "Nothing found in db";
+								                    }
+								                    //mysqli_close($conn);
+								                ?>	
+
+											</div>
+
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
