9ac7ef0e975dca3fc9f7aac71f1931c6abd14da1
abir <61460970+abirm74@users.noreply.github.com>
2021-01-03 13:33:38 +0100

    main page

---
+++ view/front/footer.php
@@ -0,0 +1,57 @@
+		<footer id="colorlib-footer" role="contentinfo">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget">
+						<h4>Atlas Travel </h4>
+						<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+						<p>
+							<ul class="colorlib-social-icons">
+								<li><a href="http://twitter.com/iam_kih"><i class="icon-twitter"></i></a></li>
+								<li><a href="http://facebook.com/iamkih"><i class="icon-facebook"></i></a></li>
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-2 colorlib-widget">
+						<h4>Book Now</h4>
+						<p>
+							<ul class="colorlib-footer-links">
+								<li><a href="hotels.php">Hotels</a></li>
+								<li><a href="tours.php">Tour</a></li>
+								<li><a href="about.php">About</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-2 colorlib-widget">
+						<h4>Pinned Hotels</h4>
+						<p>
+							<ul class="colorlib-footer-links">
+							
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-2">
+						<h4>Pinned Posts</h4>
+						<ul class="colorlib-footer-links">
+						
+						</ul>
+					</div>
+
+					<div class="col-md-3 col-md-push-1">
+						<h4>Contact Information</h4>
+						<ul class="colorlib-footer-links">
+						
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Developed By <a >Colorlib</a>
+
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
