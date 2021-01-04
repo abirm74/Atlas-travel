3fb080fe6985d2a1746a4099ce31633ac2d4d24e
abir <61460970+abirm74@users.noreply.github.com>
2021-01-04 18:57:30 +0100

    another commit

---
+++ back/production/post-new.php
@@ -0,0 +1,381 @@
+<?php
+if(!isset($_SESSION)) { session_start(); }
+function data_sanitization($data)
+{
+	$data= trim($data);
+	$data= stripcslashes($data);
+	$data= htmlspecialchars($data);
+	return $data;
+}
+
+if($_SERVER['REQUEST_METHOD']== "POST")
+{
+  require 'config.php';
+  
+  $uniqueID = md5(rand() * time());
+  $target_dir = "dist/img/";
+  $target_file = $target_dir .$uniqueID. basename($_FILES["user_img"]["name"]);
+  move_uploaded_file($_FILES["user_img"]["tmp_name"], $target_file);
+
+  $cat_id=data_sanitization($_POST['cat_id']);
+  $post_title=data_sanitization($_POST['post_title']);
+  $post_desc= mysqli_real_escape_string($conn, $_POST['post_desc']);
+  $addedBy= $_SESSION['user'];
+ 
+  $statement="insert into posts(cat_id,title,description,addedBy,image) values ('$cat_id','$post_title', '$post_desc', '$addedBy', '$target_file')";
+  
+  if(mysqli_query($conn,$statement))
+  {
+    $notifyMsg="New Post Added";
+  }
+  else
+  {
+    $notifyMsg="Unable to add New Post";
+    mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="images/ahmed.ico" type="image/ico" />
+
+    <title>Atlas! | </title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+	
+    <!-- bootstrap-progressbar -->
+    <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.php" class="site_title"><img src="images/ahmed.ico"> <span>Atlas Travel </span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Ayedi Ahmed</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="index.php"><i class="fa fa-home"></i>Home</a>
+                   
+                  </li>
+                  <li><a href="#"><i class="fa fa-home"></i>Users</a>
+                   
+                  </li>
+                  <li><a href="#"><i class="fa fa-home"></i>Hotels</a>
+                   
+                  </li>
+                  <li><a href="#"><i class="fa fa-home"></i>Reservation</a>
+                   
+                  </li>
+                  <li><a><i class="fa fa-desktop"></i>Blog <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      
+                    <li><a href="all-posts.php">All Posts</a></li>
+                    <li><a href="post-new.php">Add New</a></li> 
+                      <li><a href="categories.php">Categories</a></li> 
+                      <li><a href="add-categories.php">Add Categorie </a></li>
+                    </ul>
+                  </li>         
+                </ul>
+              </div>   
+                 </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+              </a>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+              <nav class="nav navbar-nav">
+              <ul class=" navbar-right">
+                <li class="nav-item dropdown open" style="padding-left: 15px;">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">Ahmed Ayedi
+                  </a>
+                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item"  href="javascript:;"> Profile</a>
+                      <a class="dropdown-item"  href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                  <a class="dropdown-item"  href="javascript:;">Help</a>
+                    <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  </div>
+                </li>
+
+                <li role="presentation" class="nav-item dropdown open">
+                  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>
+                  </a>
+                  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="text-center">
+                        <a class="dropdown-item">
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <!-- top tiles -->
+         
+          <!-- /top tiles -->
+
+          <div class="row">
+            <div class="col-md-12 col-sm-12 ">
+              <div class="dashboard_graph">
+
+                <div class="row x_title">
+                  <div class="col-md-6">
+                    <h3>Atlas Travel <small>Welcome </small></h3>
+                  </div>
+               
+                </div>
+
+               <!-- INSERT CODE HERE--> 
+            
+            <div class="box-header with-border">
+              <h3 class="box-title">Add New</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" name="room-type-new" method="post" action="" enctype="multipart/form-data">
+              <div class="box-body">
+
+
+                <div class="form-group">
+                  <label>Title</label>
+                  <input type="text" class="form-control" id="post_title" placeholder="Post Title" name="post_title" required>
+                </div>
+			          <div class="form-group">
+                  <label>Categoroy</label>
+                  <select class="form-control" name="cat_id">
+                    <option value="">-SELECT-</option>
+                  <?php
+                      require 'config.php';
+
+                      $statement="select cat_id, title from categories where deletedAt is NULL";
+                      $result = mysqli_query($conn, $statement);
+
+                      if (mysqli_num_rows($result) > 0)
+                      {
+                          while($row = mysqli_fetch_assoc($result))
+                          {
+                            echo "<option value=\"$row[cat_id]\">$row[title]</option>";
+                          }
+                      }
+                      else
+                      {
+                          echo "Nothing found in db";
+                      }
+                      mysqli_close($conn);
+                  ?>
+                  </select>
+                </div>
+               <div class="form-group">
+                  <label>Post Description</label>
+                  <textarea class="form-control" rows="3" id="packageDesc" placeholder="Post Description" name="post_desc"></textarea>
+                </div> 
+				
+				
+				      <div class="form-group">
+                  <label for="user-img">Upload A Photo</label>
+                  <input type="file" id="user-img" name="user_img">
+                </div>            
+              
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+
+               <div class="error">
+                 <?php
+
+                   if (!empty($notifyMsg)) 
+                   {
+                    echo "<p><span id=\"error\">$notifyMsg</span></p>";
+                   }
+
+                 ?>
+              </div>
+
+                <button type="submit" class="btn btn-primary">ADD</button>
+              </div>
+            </form>
+          </div>
+
+                <div class="clearfix"></div>
+              </div>
+            </div>
+
+          </div>
+          <br />
+    
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Atlas<a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="../vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="../vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="../vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="../vendors/Flot/jquery.flot.js"></script>
+    <script src="../vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="../vendors/Flot/jquery.flot.time.js"></script>
+    <script src="../vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="../vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="../vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="../vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="../vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="../vendors/moment/min/moment.min.js"></script>
+    <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+	
+  </body>
+</html>
